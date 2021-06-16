6b7f5e791fced58c718dcf0cc8e6cef7e60bdeaf
Virtual- <luke@tonnochi.net>
2021-06-17 00:25:56 +0100

    Added WoW plugin

---
+++ cogs/wow.py
@@ -0,0 +1,30 @@
+import requests
+import discord
+from discord.ext import commands
+from bs4 import BeautifulSoup
+
+class WoW(commands.Cog):
+    """This class/cog is responsible for searching Warcraft items and pasting into chat"""
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.command()
+    async def wow(self, ctx, *, search):
+        url = 'https://www.wowdb.com/search?search='
+        query = search.replace(' ', '+')
+
+        try:
+            page = requests.get(url + query)
+            soup = BeautifulSoup(page, 'html.parser')
+            result = soup.find(class_="even")
+            end_url = str(result.select_one("a")['href'])
+
+            await ctx.send(end_url)
+        except:
+            await ctx.send('Failed to find that page, sorry.')
+
+
+
+def setup(bot):
+    bot.add_cog(WoW(bot))
