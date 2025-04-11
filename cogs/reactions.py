00def703441040af0e5b12cca59c12acd9461fe7
Virtual- <luke@tonnochi.net>
2025-04-11 23:55:56 +0100

    Reworked reactions to use a local database for dynamic adding of images

--- cogs/reactions.py
+++ cogs/reactions.py
@@ -1,39 +1,60 @@
 import discord
+import sqlite3
+import requests
 from discord.ext import commands
+from PIL import Image
+from io import BytesIO
 
+con = sqlite3.connect('reactions.db')
+cur = con.cursor()
+cur.execute('''CREATE TABLE IF NOT EXISTS reactions
+                    (command text PRIMARY KEY, filename text)''')
+con.commit()
+con.close()
 
 class Reactions(commands.Cog):
     """This class is responsible for reactions pics and gifs"""
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command(aliases=['r'])
+    async def reaction(self, ctx, command: str):
+        con = sqlite3.connect('reactions.db')
+        cur = con.cursor()
+        cur.execute("SELECT filename FROM reactions WHERE command = ?", (command,))
+        result = cur.fetchone()
+        file_path = result[0]
+        con.close()
+        await ctx.send(file=discord.File(file_path))
 
     @commands.command()
-    async def audiophile(self, ctx):
-        """!audiophile - Places audiophile image into chat."""
-        await ctx.send(file=discord.File('assets/audiophile.jpg'))
-
-
-    @commands.command()
-    async def soy(self, ctx):
-        """!soy - Places a soyboy image into chat."""
-        await ctx.send(file=discord.File('assets/soy.png'))
-
-
-    @commands.command()
-    async def gal(self, ctx):
-        """!gal - Places galosengen into chat."""
-        await ctx.send(file=discord.File('assets/gal.gif'))
+    async def reactionlist(self, ctx):
+        con = sqlite3.connect('reactions.db')
+        cur = con.cursor()
+        cur.execute("SELECT command FROM reactions")
+        rows = cur.fetchall()
+        formatted = "\n".join(f"!r {row[0]}" for row in rows)
+        message = f'''```{formatted}```'''
+        await ctx.send(message)
+        con.close()
 
     @commands.command()
-    async def smiley(self, ctx):
-        """!smiley - Places galosengen into chat."""
-        await ctx.send(file=discord.File('assets/smiley.gif'))
+    async def reactionadd(self, ctx, name: str, url: str):
+        url = url
 
-    @commands.command()
-    async def juul(self, ctx):
-        """!juul - Places juul vape vocoded into chat."""
-        await ctx.send(file=discord.File('assets/juul.gif'))
+        response = requests.get(url)
+        img = Image.open(BytesIO(response.content))
 
+        format = img.format.lower()
+        filename = f"{name}.{format}"
+        img.save(f"assets/{filename}")
+
+        con = sqlite3.connect('reactions.db')
+        cur = con.cursor()
+        cur.execute("INSERT INTO reactions (command, filename) VALUES (?, ?)", (name, "assets/" + filename))
+        con.commit()
+        con.close()
+
+  
 async def setup(bot):
     await bot.add_cog(Reactions(bot))
